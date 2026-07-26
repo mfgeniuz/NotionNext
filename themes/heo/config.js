652d60b308f37855ea60b6399497dc7eb4c47575
Geniuz <jeffpunkie@gmail.com>
2026-07-26 09:18:23 +0800

    change: config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -6,26 +6,25 @@ const CONFIG = {
 
   HEO_INFO_CARD_AVATAR_BLUR: true, // 文章详情页个人资料卡头像样式。true：显示为模糊装饰头像；false：与首页头像保持一致
 
-  HEO_COLOR_PRIMARY: '#4f65f0',
-  HEO_COLOR_PRIMARY_HOVER: '#4f46e5',
+  HEO_COLOR_PRIMARY: '#b1ad9a',
+  HEO_COLOR_PRIMARY_HOVER: '#56533e',
   HEO_COLOR_PRIMARY_TEXT: '#ffffff',
   HEO_COLOR_ACCENT: '#dca846',
-  HEO_COLOR_BG: '#f7f9fe',
+  HEO_COLOR_BG: '#fafafa',
   HEO_COLOR_BG_DARK: '#18171d',
   HEO_COLOR_CARD: '#ffffff',
   HEO_COLOR_CARD_DARK: '#1e1e1e',
   HEO_COLOR_CARD_MUTED: '#f1f3f8',
-  HEO_COLOR_BORDER: '#4f46e5',
+  HEO_COLOR_BORDER: '#56533e',
   HEO_COLOR_BORDER_DARK: '#dca846',
   HEO_COLOR_TEXT: '#111827',
   HEO_COLOR_TEXT_SECONDARY: '#4b5563',
 
-  HEO_SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
+  HEO_SITE_CREATE_TIME: '2026-07-19', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到我的博客', url: '#' },
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -34,19 +33,19 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_1: '随便写点东西',
+  HEO_HERO_TITLE_2: '',
+  HEO_HERO_TITLE_3: '',
+  HEO_HERO_TITLE_4: '随便写点东西',
+  HEO_HERO_TITLE_5: '',
+  HEO_HERO_TITLE_LINK: '#',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
   HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_3: { title: '教程分享', url: '/tag/教程分享' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
@@ -68,11 +67,11 @@ const CONFIG = {
   ],
 
   // 个人资料底部按钮
-  HEO_INFO_CARD_URL1: '/about',
-  HEO_INFO_CARD_ICON1: 'fas fa-user',
-  HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
-  HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://www.tangly1024.com',
+  HEO_INFO_CARD_URL1: 'https://www.youtube.com/@mfgeniuz',
+  HEO_INFO_CARD_ICON1: 'fab fa-youtube',
+  HEO_INFO_CARD_URL2: 'https://open.spotify.com/user/dwe1s7j1waq61m8vd1p75ozav',
+  HEO_INFO_CARD_ICON2: 'fab fa-spotify',
+  HEO_INFO_CARD_URL3: '/about',
   HEO_INFO_CARD_TEXT3: '了解更多',
 
   // 用户技能图标
@@ -136,10 +135,11 @@ const CONFIG = {
   ],
 
   HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
-  HEO_SOCIAL_CARD_TITLE_1: '交流频道',
-  HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
-  HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_TITLE_1: 'BiliBili',
+  HEO_SOCIAL_CARD_TITLE_2: '关注我的B站账号',
+  HEO_SOCIAL_CARD_TITLE_3: '点击关注',
+  HEO_SOCIAL_CARD_URL: '#',
+
 
   // 底部统计面板文案
   HEO_POST_COUNT_TITLE: '文章数:',
